42fc2d23a74a888477eceb9fa61a94132b7d6e86
Matt <40653288+vmetric@users.noreply.github.com>
2019-01-07 10:23:09 -0700

    Add button string resource values, price arraylist, and begin button handling setup

--- app/src/main/java/net/bytesizedtech/matt/basicpos/MainActivity.java
+++ app/src/main/java/net/bytesizedtech/matt/basicpos/MainActivity.java
@@ -2,12 +2,46 @@ package net.bytesizedtech.matt.basicpos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Double> prices = new ArrayList<Double>(15){
+            {
+            add(1.21);
+            add(2.20);
+            add(3.10);
+            add(2.75);
+            add(8.75);
+            add(5.00);
+            add(1.00);
+            add(3.00);
+            add(2.21);
+            add(3.32);
+            add(5.75);
+            add(15.00);
+            add(25.00);
+            add(12.42);
+            add(9.12);
+            }
+        };
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // default method for handling onClick Events..
     }
 }
