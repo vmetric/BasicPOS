d580a0d9ffe7a7a43d994d718511cd62d9fd7c06
Matt <40653288+vmetric@users.noreply.github.com>
2019-01-07 13:05:08 -0700

    Fix pricing, fix buttons all being button 1

--- app/src/main/java/net/bytesizedtech/matt/basicpos/MainActivity.java
+++ app/src/main/java/net/bytesizedtech/matt/basicpos/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView display = findViewById(R.id.display);
-    double runningTotal = 0;
+    TextView display;
+    double runningTotal;
     ArrayList<Double> prices = new ArrayList<Double>(15){
         {
             add(1.21);
@@ -36,35 +36,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        display = findViewById(R.id.display);
+        runningTotal = 0.0;
+
         Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(this);
-        Button button2 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(this);
-        Button button3 = findViewById(R.id.button1);
+        Button button3 = findViewById(R.id.button3);
         button3.setOnClickListener(this);
-        Button button4 = findViewById(R.id.button1);
+        Button button4 = findViewById(R.id.button4);
         button4.setOnClickListener(this);
-        Button button5 = findViewById(R.id.button1);
+        Button button5 = findViewById(R.id.button5);
         button5.setOnClickListener(this);
-        Button button6 = findViewById(R.id.button1);
+        Button button6 = findViewById(R.id.button6);
         button6.setOnClickListener(this);
-        Button button7 = findViewById(R.id.button1);
+        Button button7 = findViewById(R.id.button7);
         button7.setOnClickListener(this);
-        Button button8 = findViewById(R.id.button1);
+        Button button8 = findViewById(R.id.button8);
         button8.setOnClickListener(this);
-        Button button9 = findViewById(R.id.button1);
+        Button button9 = findViewById(R.id.button9);
         button9.setOnClickListener(this);
-        Button button10 = findViewById(R.id.button1);
+        Button button10 = findViewById(R.id.button10);
         button10.setOnClickListener(this);
-        Button button11 = findViewById(R.id.button1);
+        Button button11 = findViewById(R.id.button11);
         button11.setOnClickListener(this);
-        Button button12 = findViewById(R.id.button1);
+        Button button12 = findViewById(R.id.button12);
         button12.setOnClickListener(this);
-        Button button13 = findViewById(R.id.button1);
+        Button button13 = findViewById(R.id.button13);
         button13.setOnClickListener(this);
-        Button button14 = findViewById(R.id.button1);
+        Button button14 = findViewById(R.id.button14);
         button14.setOnClickListener(this);
-        Button button15 = findViewById(R.id.button1);
+        Button button15 = findViewById(R.id.button15);
         button15.setOnClickListener(this);
         Button buttonClear = findViewById(R.id.buttonClear);
         buttonClear.setOnClickListener(this);
@@ -78,77 +81,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()){
             case R.id.button1:
-                runningTotal = runningTotal + prices.get(1);
+                runningTotal = runningTotal + prices.get(0);
                 updateDisplay();
                 break;
 
             case R.id.button2:
-                runningTotal = runningTotal + prices.get(2);
+                runningTotal = runningTotal + prices.get(1);
                 updateDisplay();
                 break;
 
             case R.id.button3:
-                runningTotal = runningTotal + prices.get(3);
+                runningTotal = runningTotal + prices.get(2);
                 updateDisplay();
                 break;
 
             case R.id.button4:
-                runningTotal = runningTotal + prices.get(4);
+                runningTotal = runningTotal + prices.get(3);
                 updateDisplay();
                 break;
 
             case R.id.button5:
-                runningTotal = runningTotal + prices.get(5);
+                runningTotal = runningTotal + prices.get(4);
                 updateDisplay();
                 break;
 
             case R.id.button6:
-                runningTotal = runningTotal + prices.get(6);
+                runningTotal = runningTotal + prices.get(5);
                 updateDisplay();
                 break;
 
             case R.id.button7:
-                runningTotal = runningTotal + prices.get(7);
+                runningTotal = runningTotal + prices.get(6);
                 updateDisplay();
                 break;
 
             case R.id.button8:
-                runningTotal = runningTotal + prices.get(8);
+                runningTotal = runningTotal + prices.get(7);
                 updateDisplay();
                 break;
 
             case R.id.button9:
-                runningTotal = runningTotal + prices.get(9);
+                runningTotal = runningTotal + prices.get(8);
                 updateDisplay();
                 break;
 
             case R.id.button10:
-                runningTotal = runningTotal + prices.get(10);
+                runningTotal = runningTotal + prices.get(9);
                 updateDisplay();
                 break;
 
             case R.id.button11:
-                runningTotal = runningTotal + prices.get(11);
+                runningTotal = runningTotal + prices.get(10);
                 updateDisplay();
                 break;
 
             case R.id.button12:
-                runningTotal = runningTotal + prices.get(12);
+                runningTotal = runningTotal + prices.get(11);
                 updateDisplay();
                 break;
 
             case R.id.button13:
-                runningTotal = runningTotal + prices.get(13);
+                runningTotal = runningTotal + prices.get(12);
                 updateDisplay();
                 break;
 
             case R.id.button14:
-                runningTotal = runningTotal + prices.get(14);
+                runningTotal = runningTotal + prices.get(13);
                 updateDisplay();
                 break;
 
             case R.id.button15:
-                runningTotal = runningTotal + prices.get(15);
+                runningTotal = runningTotal + prices.get(14);
                 updateDisplay();
                 break;
 
